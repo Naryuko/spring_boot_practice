617d341734f7a7a931d824a1e85201d5f9bfda1f
Naryuko <lion09873@naver.com>
2021-04-12 23:26:57 +0900

    api 서버와 같이 json 보내는 기본 방법 공부

--- demo/src/main/java/com/Naryu/demo/controller/HelloController.java
+++ demo/src/main/java/com/Naryu/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,5 +17,31 @@ public class HelloController {
     public String helloMVC(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http의 헤더와 body 중 body에 이 내용을 직접 넣어주겠다는 의미
+    // 이전과 다르게 view를 거치지 않고 그냥 이 문자가 그대로 보내진다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // ResponseBody 선언 이후 객체를 반환하면 기본으로 json으로 반환해준다.
+    public Hello helloAPI (@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
