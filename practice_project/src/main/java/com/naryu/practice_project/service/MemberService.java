d27479d63c67e8d863822b089d0616a3764912d9
Naryuko <lion09873@naver.com>
2021-04-13 00:21:38 +0900

    service 패키지에서 회원가입, 회원 조희 구현

---
+++ practice_project/src/main/java/com/naryu/practice_project/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.naryu.practice_project.service;
+
+import com.naryu.practice_project.domain.Member;
+import com.naryu.practice_project.repository.MemberRepository;
+import com.naryu.practice_project.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원가입
+    public Long join (Member member) {
+        // 중복 이름의 회원은 안 된다.
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
+
